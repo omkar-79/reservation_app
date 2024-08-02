53a9d8fc499da857a1a6629e8dbd3c70810b03c1
omkar-79 <omkarbalekundri79@gmail.com>
2024-08-01 22:35:45 -0400

    Bouncing icons and search baver hover

---
+++ frontend/src/assets/icons/index.js
@@ -0,0 +1,13 @@
+// src/assets/icons/index.js
+
+import tennisIcon from '../icons/tennis.png';
+import basketballIcon from '../icons/basketball.png';
+// Import additional icons as needed
+
+const icons = {
+  tennis: tennisIcon,
+  basketball: basketballIcon,
+  // Add more icon mappings here
+};
+
+export default icons;
--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,9 +1,10 @@
-import React from 'react';
-import { Container, Navbar, Nav } from 'react-bootstrap';
+import React, { useState } from 'react';
+import { Container, Navbar, Nav, Button, FormControl } from 'react-bootstrap';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import L from 'leaflet';
 import '../css/HomePage.css';
+import icons from '../assets/icons'; // Import the icons
 
 // Fix for default marker icon not showing in Leaflet
 delete L.Icon.Default.prototype._getIconUrl;
@@ -13,10 +14,19 @@ L.Icon.Default.mergeOptions({
   shadowUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-shadow.png',
 });
 
+// Custom bouncing icon
+const createIcon = (iconUrl) => L.divIcon({
+  className: 'bouncing-icon',
+  html: `<img src="${iconUrl}" class="custom-icon" />`,
+  iconSize: [32, 32],
+});
+
 const HomePage = () => {
+  const [searchValue, setSearchValue] = useState('');
+
   const grounds = [
-    { id: 1, name: 'Ground 1', lat: 38.89511, lng: -77.03637 },
-    { id: 2, name: 'Ground 2', lat: 38.88951, lng: -77.05012 },
+    { id: 1, name: 'Tennis Court 1', type: 'tennis', lat: 38.89511, lng: -77.03637, courtsAvailable: 2 },
+    { id: 2, name: 'Basketball Court 1', type: 'basketball', lat: 38.88951, lng: -77.05012, courtsAvailable: 1 },
     // Add more grounds here...
   ];
 
@@ -40,17 +50,40 @@ const HomePage = () => {
       </Navbar>
 
       {/* Full-Screen Map */}
-      <MapContainer center={[38.89511, -77.03637]} zoom={13} className="map" style={{ height: '100vh', width: '100%' }}>
-        <TileLayer
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          attribution="&copy; <a href='http://osm.org/copyright'>OpenStreetMap</a> contributors"
+      <div className="map-container">
+        <MapContainer center={[38.89511, -77.03637]} zoom={13} className="map">
+          <TileLayer
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            attribution="&copy; <a href='http://osm.org/copyright'>OpenStreetMap</a> contributors"
+          />
+          {grounds
+            .filter((ground) => ground.name.toLowerCase().includes(searchValue.toLowerCase()))
+            .map((ground) => (
+              <Marker
+                key={ground.id}
+                position={[ground.lat, ground.lng]}
+                icon={createIcon(icons[ground.type])} // Use the appropriate icon
+              >
+                <Popup>
+                  <div>
+                    <h4>{ground.name}</h4>
+                    <p>Courts Available: {ground.courtsAvailable}</p>
+                    <Button variant="primary">Reserve</Button>
+                  </div>
+                </Popup>
+              </Marker>
+            ))}
+        </MapContainer>
+
+        {/* Search Bar */}
+        <FormControl
+          type="text"
+          placeholder="Search"
+          className="search-bar"
+          value={searchValue}
+          onChange={(e) => setSearchValue(e.target.value)}
         />
-        {grounds.map((ground) => (
-          <Marker key={ground.id} position={[ground.lat, ground.lng]}>
-            <Popup>{ground.name}</Popup>
-          </Marker>
-        ))}
-      </MapContainer>
+      </div>
 
       {/* Footer */}
       <footer className="footer">
